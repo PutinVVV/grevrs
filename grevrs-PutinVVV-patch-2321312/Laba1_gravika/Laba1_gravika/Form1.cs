7c9ca09b3a81e6c65672d8a686086d0f8c61474c
PutinVVV <144924225+PutinVVV@users.noreply.github.com>
2024-03-03 12:57:06 +0300

    Add files via upload

---
+++ grevrs-PutinVVV-patch-2321312/Laba1_gravika/Laba1_gravika/Form1.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Laba1_gravika
+{
+
+    public partial class Form1 : Form
+    {
+        Bitmap image;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void открытьToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "image files|*.png;*.jpg;*.bmp|All files(*.*)|*.*";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                image = new Bitmap(dialog.FileName);
+                pictureBox1.Image = image;
+                pictureBox1.Refresh();
+            }
+            
+        }
+
+        private void инверсияToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Filters filter = new InvertFilter();
+            backgroundWorker1.RunWorkerAsync(filter);
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Bitmap newImage = ((Filters)e.Argument).processImage(image, backgroundWorker1);
+            if (backgroundWorker1.CancellationPending != true)
+                image = newImage;
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (!e.Cancelled)
+            {
+                pictureBox1.Image = image;
+                pictureBox1.Refresh();
+            }
+            progressBar1.Value = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            backgroundWorker1.CancelAsync();
+        }
+
+        private void размытиеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Filters filter = new BlueFilter();
+            backgroundWorker1.RunWorkerAsync(filter);
+        }
+
+        private void фильтрГаусаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Filters filter = new GausFilter();
+            backgroundWorker1.RunWorkerAsync(filter);
+        }
+
+        private void grayScaleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Filters filter = new GrayS();
+            backgroundWorker1.RunWorkerAsync(filter);
+        }
+
+        private void сепияToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Filters filter = new Sepia();
+            backgroundWorker1.RunWorkerAsync(filter);
+        }
+
+        private void повыситьЯркостьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Filters filter = new Yarkost();
+            backgroundWorker1.RunWorkerAsync(filter);
+        }
+
+        private void фильтрСобеляToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Filters filter = new Sobel();
+            backgroundWorker1.RunWorkerAsync(filter);
+        }
+
+        private void повыситьРезкостьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Filters filter = new Rezkost();
+            backgroundWorker1.RunWorkerAsync(filter);
+        }
+    }
+    public abstract class Filters
+    {
+        protected abstract Color calculateNewPixelColor(Bitmap sourceImage, int x, int y);
+        public Bitmap processImage(Bitmap sourceImage, BackgroundWorker worker)
+        {
+            Bitmap resultImage = new Bitmap(sourceImage.Width, sourceImage.Height);
+            for (int i = 0; i < sourceImage.Width; i++)
+                for (int j = 0; j < sourceImage.Height; j++)
+                {
+                    worker.ReportProgress((int)((float)i/resultImage.Width*100));
+                    if (worker.CancellationPending)
+                        return null;
+                    resultImage.SetPixel(i, j, calculateNewPixelColor(sourceImage, i, j));
+                }
+            return resultImage;
+        }
+        public int Clamp(int val, int min, int max)
+        {
+            if (val < min)
+                return min;
+            if (val > max)
+                return max;
+            return val;
+        }
+    }
+    public class InvertFilter : Filters
+    {
+        protected override Color calculateNewPixelColor(Bitmap sourceImage, int x, int y)
+        {
+            Color sourceColor = sourceImage.GetPixel(x, y);
+            Color resultColor = Color.FromArgb(255 - sourceColor.R, 255 - sourceColor.G, 255 - sourceColor.B);
+            return resultColor;
+        }
+    }
+
+    public class GrayS : Filters
+    {
+        protected override Color calculateNewPixelColor(Bitmap sourceImage, int x, int y)
+        {
+            Color sourceColor = sourceImage.GetPixel(x, y);
+            double red = sourceColor.R;
+            double blue = sourceColor.B;
+            double green = sourceColor.G;
+            int res =(int) Math.Round(red * 0.299+0.587*green+0.114*blue);
+            Color resultColor = Color.FromArgb(res, res, res);
+            return resultColor;
+        }
+    }
+
+    public class Sepia : Filters
+    {
+        protected override Color calculateNewPixelColor(Bitmap sourceImage, int x, int y)
+        {
+            Color sourceColor = sourceImage.GetPixel(x, y);
+            double red = sourceColor.R;
+            double blue = sourceColor.B;
+            double green = sourceColor.G;
+            int res = (int) Math.Round(red * 0.299 + 0.587 * green + 0.114 * blue);
+            Color resultColor = Color.FromArgb(Clamp(res+2*50,0,255), Clamp(res + 25 , 0, 255), Clamp(res - 1 * 50, 0, 255));
+            return resultColor;
+        }
+    }
+
+    public class Yarkost : Filters
+    {
+        protected override Color calculateNewPixelColor(Bitmap sourceImage, int x, int y)
+        {
+            Color sourceColor = sourceImage.GetPixel(x, y);
+            Color resultColor = Color.FromArgb(Clamp(sourceColor.R+10,0,255), Clamp(sourceColor.G + 10, 0, 255), Clamp(sourceColor.B + 10, 0, 255));
+            return resultColor;
+        }
+    }
+
+    class MatrixFilter : Filters
+    {
+        protected float[,] kernel = null;
+        protected MatrixFilter() { }
+        public MatrixFilter(float[,] kernel)
+        {
+            this.kernel = kernel;
+        }
+        protected override Color calculateNewPixelColor(Bitmap sourceImage, int x, int y)
+        {
+            int Xradius = kernel.GetLength(0) / 2;
+            int Yradius = kernel.GetLength(1) / 2;
+            float Rres = 0, Bres = 0, Gres = 0;
+            for (int l = -Yradius; l <= Yradius; l++)
+                for (int k = -Xradius; k <= Xradius; k++)
+                {
+                    int idx = Clamp(x + k, 0, sourceImage.Width - 1);
+                    int idy = Clamp(y + l, 0, sourceImage.Height - 1);
+                    Color neighborColor = sourceImage.GetPixel(idx, idy);
+                    Rres += neighborColor.R * kernel[k + Xradius, l + Yradius];
+                    Gres += neighborColor.G * kernel[k + Xradius, l + Yradius];
+                    Bres += neighborColor.B * kernel[k + Xradius, l + Yradius];
+                }
+            return Color.FromArgb(Clamp((int)Rres, 0, 255), Clamp((int)Gres, 0, 255), Clamp((int)Bres, 0, 255));
+
+        }
+    }
+    class BlueFilter : MatrixFilter
+    {
+        public BlueFilter()
+        {
+            int sizeX = 3;
+            int sizeY = 3;
+            kernel = new float[sizeX, sizeY];
+            for (int i = 0; i < sizeX; i++)
+                for (int j = 0; j < sizeY; j++)
+                    kernel[i, j] = 1.0f / (float)(sizeX * sizeY);
+        }
+    }
+    class GausFilter : MatrixFilter
+    {
+        public void createGausKernel(int radius, float sigma)
+        {
+            int size = 2 * radius + 1;
+            kernel = new float[size, size];
+            float norm = 0;
+            for (int i = -radius; i <= radius; i++)
+                for (int j = -radius; j <= radius; j++)
+                {
+                    kernel[i + radius, j + radius] = (float)Math.Exp(-(i * i + j * j) / (2 * sigma * sigma));
+                    norm += kernel[i + radius, j + radius];
+                }
+            for (int i = 0; i < size; i++)
+                for (int j = 0; j < size; j++)
+                    kernel[i, j] /= norm;
+        }
+        public GausFilter()
+        { createGausKernel(3, 2); }
+    }
+
+    class Sobel : MatrixFilter
+    {
+        public Sobel()
+        {
+            int size = 3;
+            kernel = new float[size, size];
+            kernel[0 ,0]= -1; kernel[0, 1] = -2; kernel[0, 2] = -1;
+            kernel[1, 0] = 0; kernel[1, 1] = 0; kernel[1, 2] = 0;
+            kernel[2, 0] = 1; kernel[2, 1] = 2; kernel[2, 2] = 1;
+        }
+
+    }
+
+    class Rezkost : MatrixFilter
+    {
+        public Rezkost()
+        {
+            int size = 3;
+            kernel = new float[size, size];
+            kernel[0, 0] = 0; kernel[0, 1] = -1; kernel[0, 2] = 0;
+            kernel[1, 0] = -1; kernel[1, 1] = 5; kernel[1, 2] = -1;
+            kernel[2, 0] = 0; kernel[2, 1] = -1; kernel[2, 2] = 0;
+        }
+
+    }
+}
+
